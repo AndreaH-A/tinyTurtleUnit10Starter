e58ab2138b92825b45f00b97c48f843ef7797e31
Andrea Hernandez Aguilar <andrea.hdz.aguilar@gmail.com>
2017-01-31 23:43:15 +0000

    no fun

--- script.js
+++ script.js
@@ -12,7 +12,61 @@ right(90);
 forward(50);
 }
 
+function triangle(){
+right(45);
+forward(35);
+right(90);
+forward(35);
+}
+
+function house(){
+forward(50);
+right(90);
+forward(50);
+right(90);
+forward(50);
+right(90);
+forward(50);
+right(90);
+forward(50);
+right(45);
+forward(35);
+right(90);
+forward(35);
+}
+
+function pentagon(){
+right(72);
+forward(35);
+right(72);
+forward(35);
+right(72);
+forward(35);
+right(72);
+forward(35);
+right(72);
+forward(35);
+}
+
+function hexagon(){
+forward(50);
+right(60);
+forward(50);
+right(60);
+forward(50);
+right(60);
+forward(50);
+right(60);
+forward(50);
+right(60);
+forward(50);
+right(60);
+}
+
 // Type your function call below
-
-
+//square();
 stamp();
+//triangle();
+//house();
+//pentagon();
+hexagon();
